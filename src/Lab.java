3cb4256d6547f12ca85a69fe4bd12aea4454e614
Max Bondarchuk <bondarchuk.m.y@gmail.com>
2016-02-21 18:15:43 +0200

    Small update

--- src/Lab.java
+++ src/Lab.java
@@ -1,12 +1,10 @@
 public class Lab {
     public int GetLettersAndPrintStuff(int number) {
-//        System.out.print("Give me integer\n> ");
         int x = 0;
         while (number != 0) {
             x += number % 10;
             number /= 10;
         }
-//        System.out.println(String.format("X = %d", x));
         double squareRootOfX = Math.sqrt(x);
         System.out.println(String.format("Square root of x = %f", squareRootOfX));
         System.out.println(String.format("Rounded square root of x = %d", Math.round(squareRootOfX)));
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Lab lab = new Lab();    // Java is just amazing
         Scanner keyboard = new Scanner(System.in);
-        System.out.print("Give me integer\n> ");
+        System.out.print("Give me an integer\n> ");
         try {
             lab.GetLettersAndPrintStuff(keyboard.nextInt());
         } catch (Exception e) {
